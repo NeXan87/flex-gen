5888b873ce7915f182afd0e8020a21fc7e4bca39
NeXan <nexan87@gmail.com>
2022-09-06 22:15:49 +0300

    #

--- script.js
+++ script.js
@@ -1,78 +1,87 @@
+"use script"
+
+let boxParameters = document.querySelectorAll('.oninput');
 let elements = document.querySelector('.elements');
-let flex = document.querySelector('.item');
 let ButtonRemove = document.querySelector('.remove');
 let flexGrow = document.querySelectorAll('.flex-grow');
 let flexShrink = document.querySelectorAll('.flex-shrink');
 let flexBasis = document.querySelectorAll('.flex-basis');
-let idElement = 1;
-let item;
+let idElement = 1, str;
 
-// const idNum = Symbol('idNumber');
-// const inputAttribute = {
-// 	type: 'number',
-// 	class: 'number',
-// 	id: 'flex-grow-element-',
-// 	[idNum]: 1,
-// };
+const inputParameters = {
+	parent: {
+		"width-container": 500,
+		"height-container": 90,
+		"width-parent": "%",
+		"height-parent": "vh",
+		"flex-direction": "row",
+		"flex-wrap": "nowrap",
+		"justify-content": "flex-start",
+		"align-items": "stretch",
+		"align-content": "stretch",
+	},
+	"element-1": {},
+};
 
-// function addElement() {
-// 	let label = document.createElement('label');
-// 	let input = document.createElement('input');
-// 	label.innerHTML = `Flex-grow элемента ${++inputAttribute[idNum]}`;
-// 	label.setAttribute('for', `flex-grow-element-${inputAttribute[idNum]}`);
-// 	setInputAttribute(input);
-// 	elements = flexElement;
-// }
-
-// function setInputAttribute(input) {
-// 	for (let attribute in inputAttribute) {
-// 		if (attribute !== 'id') {
-// 			input.setAttribute(attribute, inputAttribute[attribute]);
-// 		} else {
-// 			input.setAttribute(attribute, `${inputAttribute[attribute]}${inputAttribute[idNum]}`);
-// 		}
-// 	}
-// }
+addToInputParameters();
 
 function addElement() {
 
-	if ( idElement < 10 ) {
+	if (idElement < 10) {
 		idElement++;
 		let fieldset = document.createElement('fieldset');
 		fieldset.classList.add('flex', 'item');
-		fieldset.innerHTML = `<legend>Элемент ${ idElement }</legend>
-					<label for="flex-grow-element-${ idElement }" class="flex-title element">Flex-grow</label>
-					<button type="button" class="button-element remove" onclick="removeElement(this)">-</button>
-					<input type="number" class="number flex-grow element" id="flex-grow-element-${ idElement }" min="0" max="100">
-					<label for="flex-shrink-element-${ idElement }" class="flex-title element">Flex-shrink</label>
-					<input type="number" class="number flex-shrink element" id="flex-shrink-element-${ idElement }" min="0" max="100">
-					<label for="flex-basis-element-${ idElement }" class="flex-title element">Flex-basis</label>
-					<input type="number" class="number flex-basis element" id="flex-basis-element-${ idElement }" min="0" max="1000">
-					<select name="unit" id="flex-basis-unit-${ idElement }" class="unit element">
-						<option value="%" selected>%</option>
-						<option value="px">px</option>
-					</select>`;
+		fieldset.innerHTML = `<legend>Элемент ${idElement}</legend>
+									 <label for="flex-grow-element-${idElement}" class="element">flex-grow</label>
+									 <button type="button" class="button-element remove" onclick="removeElement(this)">-</button>
+									 <input type="number" class="number flex-grow element element-${idElement} oninput input-child" id="flex-grow-${idElement}" min="0" max="100">
+									 <label for="flex-shrink-element-${idElement}" class="flex-title element">flex-shrink</label>
+									 <input type="number" class="number flex-shrink element element-${idElement} oninput input-child" id="flex-shrink-${idElement}" min="0" max="100">
+									 <label for="flex-basis-element-${idElement}" class="flex-title element">flex-basis</label>
+									 <input type="number" class="number flex-basis element element-${idElement} oninput input-child" id="flex-basis-${idElement}" min="0" max="1000">
+									 <select name="unit" id="flex-basis-unit-${idElement}" class="unit element element-${idElement} oninput input-child">
+									 	<option value="%">%</option>
+									 	<option value="px" selected>px</option>
+									 </select>`;
 		elements.appendChild(fieldset);
+		inputParameters[`element-${idElement}`] = {};
 		updateItems();
 	}
 }
 
 function removeElement(input) {
-	idElement--;
+	delete inputParameters[`element-${idElement}`];
 	elements.removeChild(input.parentNode);
+	idElement--;
 	updateItems();
+	console.log(inputParameters);
 }
 
 function updateItems() {
-	item = document.querySelectorAll('.item');
+	boxParameters = document.querySelectorAll('.oninput');
 	flexGrow = document.querySelectorAll('.flex-grow');
 	flexShrink = document.querySelectorAll('.flex-shrink');
 	flexBasis = document.querySelectorAll('.flex-basis');
-	console.log(flexGrow);
-	console.log(flexShrink);
-	console.log(flexBasis);
+	addToInputParameters();
 }
 
+function addToInputParameters() {
+	for (let boxPatameter of boxParameters) {
+		boxPatameter.oninput = function () {
+			if (boxPatameter.classList.contains('input-parent')) {
+				inputParameters.parent[boxPatameter.getAttribute('id')] = boxPatameter.value;
+			}
+			if (boxPatameter.classList.contains('input-child')) {
+				for (let i = 1; i <= idElement; i++) {
+					if (+boxPatameter.getAttribute('id').replace(/[^0-9]/g, '') === i) {
+						inputParameters[`element-${i}`][boxPatameter.getAttribute('id')] = boxPatameter.value;
+						console.log(inputParameters[`element-${i}`]);
+					}
+				}
+			}
+		}
+	}
+}
 
 
 // setInterval(function() {
