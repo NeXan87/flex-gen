b8c59e4716e8b7bfc7b408e77cf71df8733dfac3
NeXan <nexan87@gmail.com>
2022-09-07 15:35:02 +0300

    #

--- script.js
+++ script.js
@@ -32,22 +32,22 @@ function addElement() {
 		fieldset.innerHTML = `<legend>Элемент ${idElement + 1}</legend>
 									 <label for="flex-grow-element-${idElement}" class="element">flex-grow</label>
 									 <div class="button-background" onclick="removeElement(this)">
-									 	<button type="button" class="button-element remove"></button>
+									 	 <button type="button" class="button-element remove"></button>
 									 </div>
 									 <input type="number" class="number flex-grow element element-${idElement} oninput input-child" id="flex-grow-${idElement}" min="0" max="100">
 									 <label for="flex-shrink-element-${idElement}" class="flex-title element">flex-shrink</label>
 									 <input type="number" class="number flex-shrink element element-${idElement} oninput input-child" id="flex-shrink-${idElement}" min="0" max="100">
 									 <label for="flex-basis-element-${idElement}" class="flex-title element">flex-basis</label>
 									 <input type="text" class="number flex-basis element element-${idElement} oninput input-child" id="flex-basis-${idElement}" min="0" max="1000">
-									 <select name="unit" id="flex-basis-unit-${idElement}" class="unit element element-${idElement} oninput input-child">
-									 	<option value="%">%</option>
-									 	<option value="px" selected>px</option>
-									 </select>
 									 <label for="margin-element-${idElement}" class="flex-title element">margin</label>
 									 <input type="text" class="number margin-top element element-${idElement} oninput input-child" id="margin-top-${idElement}" placeholder="top">
 									 <input type="text" class="number margin-right element element-${idElement} oninput input-child" id="margin-right-${idElement}" placeholder="right">
 									 <input type="text" class="number margin-bottom element element-${idElement} oninput input-child" id="margin-bottom-${idElement}" placeholder="bottom">
-									 <input type="text" class="number margin-left element element-${idElement} oninput input-child" id="margin-left-${idElement}" placeholder="bottom">`;
+									 <input type="text" class="number margin-left element element-${idElement} oninput input-child" id="margin-left-${idElement}" placeholder="bottom">
+									 <div class="result">
+										 <span>ИРС: <output name="result" class="s-compression-${idElement}">0</output></span>
+										 <span>ИРР: <output name="result" class="s-extension-${idElement}">0</output></span>
+									 </div>`;
 		elements.appendChild(fieldset);
 		inputParameters[`element-${idElement}`] = {};
 		updateItems();
@@ -120,38 +120,12 @@ function joinWidthOrHeight(boxPatameter) {
 
 function joinFlexBasis(boxPatameter) {
 
-	outher: for (let i = 0; i <= idElement; i++) {
+	for (let i = 0; i <= idElement; i++) {
 
-		if (+boxPatameter.getAttribute('id').replace(/[^0-9]/g, '') === i && boxPatameter.getAttribute('id').slice(0, -2) !== "flex-basis-unit") {
+		if (+boxPatameter.getAttribute('id').replace(/[^0-9]/g, '') === i) {
 			inputParameters[`element-${i}`][boxPatameter.getAttribute('id').slice(0, -2)] = boxPatameter.value;
 		}
 
-		if (boxPatameter.getAttribute('id').slice(0, -2) === "flex-basis") {
-			console.log(`flex-basis-unit-${i}`);
-			for (let j = 11; j < boxParameters.length; j++) {
-				console.log(j);
-				if ((boxParameters[i].getAttribute('id') === `flex-basis-unit-${j}`) && (!isNaN(+boxPatameter.value))) {
-					inputParameters[`element-${i}`][boxPatameter.getAttribute('id').slice(0, -2)] = boxPatameter.value + boxParameters[j].value;
-					console.log(j);
-					break;
-				}
-
-			}
-
-		}
-
-		if (boxPatameter.getAttribute('id').slice(0, -2) === "flex-basis-unit") {
-
-			for (let k = 11; k < boxParameters.length; k++) {
-
-				if (boxParameters[k].getAttribute('id') === `flex-basis-${i}` && !isNaN(+boxParameters[k].value)) {
-					inputParameters[`element-${i}`][`flex-basis`] = boxParameters[k].value + boxPatameter.value;
-					break outher;
-				}
-
-			}
-
-		}
 	}
 
 }
