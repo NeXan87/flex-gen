266dcf1d66aea514d466043aed86985b4f00e67c
Denis <nexan87@gmail.com>
2023-06-21 17:01:31 +0300

    Оптимизирует скрипт отображения стрелок

--- source/js/render-axes.js
+++ source/js/render-axes.js
@@ -5,22 +5,31 @@ const crossAxisClass = 'cross-coord';
 const rowReverseClass = 'row-reverse';
 const columnReverseClass = 'column-reverse';
 
+const axesElements = document.querySelectorAll('.arrow');
 const horizontalAxis = document.querySelectorAll('.horizontal-arrow');
 const verticalAxis = document.querySelectorAll('.vertical-arrow');
 
+const renameAxes = () => {
+  axesElements.forEach((axis) => {
+    const axisText = axis.querySelector('.arrow-title');
+
+    if (axis.classList.contains(mainAxisClass)) {
+      axisText.textContent = mainAxisText;
+    }
+    if (axis.classList.contains(crossAxisClass)) {
+      axisText.textContent = crossAxisText;
+    }
+  });
+};
+
 const axesCssAdd = (axes, classNames) => {
   classNames.forEach((className) => axes.forEach((axis) => axis.classList.add(className)));
+  renameAxes();
 };
 
 const axesCssRemove = (axes, classNames) => {
   classNames.forEach((className) => axes.forEach((axis) => axis.classList.remove(className)));
-};
-
-const renameAxes = (axes, text) => {
-  axes.forEach((axis) => {
-    const axisText = axis.querySelector('.arrow-title');
-    axisText.textContent = text;
-  });
+  renameAxes();
 };
 
 const renderAxes = (value) => {
@@ -30,32 +39,24 @@ const renderAxes = (value) => {
       axesCssRemove(horizontalAxis, [rowReverseClass, crossAxisClass]);
       axesCssAdd(verticalAxis, [crossAxisClass]);
       axesCssRemove(verticalAxis, [columnReverseClass, mainAxisClass]);
-      renameAxes(horizontalAxis, mainAxisText);
-      renameAxes(verticalAxis, crossAxisText);
       break;
     case 'row-reverse':
       axesCssAdd(horizontalAxis, [rowReverseClass, mainAxisClass]);
       axesCssRemove(horizontalAxis, [crossAxisClass]);
       axesCssAdd(verticalAxis, [crossAxisClass]);
       axesCssRemove(verticalAxis, [columnReverseClass, mainAxisClass]);
-      renameAxes(horizontalAxis, mainAxisText);
-      renameAxes(verticalAxis, crossAxisText);
       break;
     case 'column':
       axesCssAdd(horizontalAxis, [crossAxisClass]);
       axesCssRemove(horizontalAxis, [rowReverseClass, mainAxisClass]);
       axesCssAdd(verticalAxis, [mainAxisClass]);
       axesCssRemove(verticalAxis, [columnReverseClass, crossAxisClass]);
-      renameAxes(horizontalAxis, crossAxisText);
-      renameAxes(verticalAxis, mainAxisText);
       break;
     case 'column-reverse':
       axesCssAdd(horizontalAxis, [crossAxisClass]);
       axesCssRemove(horizontalAxis, [columnReverseClass, mainAxisClass]);
       axesCssAdd(verticalAxis, [columnReverseClass, mainAxisClass]);
       axesCssRemove(verticalAxis, [crossAxisClass]);
-      renameAxes(horizontalAxis, crossAxisText);
-      renameAxes(verticalAxis, mainAxisText);
       break;
   }
 };
