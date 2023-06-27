07eccb3de7a3d152d5df58f394dfbe2a51cc7e39
Denis <nexan87@gmail.com>
2023-06-27 17:44:59 +0300

    Добавляет файл main.js

---
+++ source/js/main.js
@@ -0,0 +1,8 @@
+import './prototypes.js';
+import { initMenuButtonActions } from './mobile-menu.js';
+import { initResizeWindowActions } from './resize-window.js';
+import { initAddItemActions } from './add-item.js';
+
+initMenuButtonActions();
+initResizeWindowActions();
+initAddItemActions();
